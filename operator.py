58622a180d46e0558439a0910576e6b5a5b112d0
Amanbnsl <136835063+Amanbnsl@users.noreply.github.com>
2023-06-17 11:44:05 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,13 @@
+op=input("enter the operator ")
+a=int(input("enter  first number "))
+b=int(input("enter second number "))
+if(op=='+'):
+    print(a+b)
+elif(op=='-'):
+    print(a-b)
+elif(op=='*'):
+    print(a*b)
+elif(op=='%'):
+    print(a%b)
+elif(op=='/'):
+    print(a/b)
