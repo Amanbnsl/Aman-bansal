f6e6e28bbabdb5d03fce464263c29e524e3e8531
Amanbnsl <136835063+Amanbnsl@users.noreply.github.com>
2023-06-17 22:20:15 +0530

    Add files via upload

---
+++ BMI of a person.py
@@ -0,0 +1,6 @@
+weight=float(input("enter the weight of the person in kg:"))
+height=float(input("enter the height of the person in metre:"))
+
+BMI=weight/(height*height)
+
+print('the BMI of the person is',BMI)
