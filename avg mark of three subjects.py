24e4585754cbec7c673d0801d8cf15b0934401ad
Amanbnsl <136835063+Amanbnsl@users.noreply.github.com>
2023-06-17 22:22:48 +0530

    Add files via upload

---
+++ avg mark of three subjects.py
@@ -0,0 +1,9 @@
+mark1=int(input("enter the marks of first subject"))
+mark2=int(input("enter the marks of second subject"))
+mark3=int(input("enter the marks of third subject"))
+
+avg=(mark1+mark2+mark3)/3
+print('the average mark of three subject is',avg)
+
+percentage=(mark1+mark2+mark3)/300*100
+print('the percentage mark is', percentage)
